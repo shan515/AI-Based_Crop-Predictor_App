ffcbf3a7c2d871131838fe736bb1791f79fcde05
KhushalPShah <khushalshah021098@gmail.com>
2020-10-29 21:07:00 +0530

    Added the API call for temperature and messsage exchange formats

--- Server/app.py
+++ Server/app.py
@@ -1,17 +1,70 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
 import joblib
-
+import csv
+import requests
+import json
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
-def index():
-    return "Hello World"
 
-@app.route('/predict',methods=['GET'])
+@app.route('/', methods=['GET'])
+def index():
+    return "Default Route"
+
+
+@app.route('/predict', methods=['POST'])
 def predict():
-    clf = joblib.load('../Models/saved_model.sav')
-    # Prediction using the extracted params to be done here
-    return "Predicted Results"
+    area = request.form.get('area')
+    temp = request.form.get('temp')
+    sowT = request.form.get('sowT')
+    pot = request.form.get('potassium')
+    nit = request.form.get('nitrogen')
+    phosphorous = request.form.get('phosphorous')
+    ph = request.form.get('ph')
+    crop_season = request.form.get('crop_season')
+    current_crop = request.form.get('current_planted_crop')
+    predict_month = request.form.get('predict_month')
+    current_crop_month = request.form.get('current_crop_month')
+
+    # Use this API for finding data using latitudes and Longitudes
+    # https://climateknowledgeportal.worldbank.org/api/data/get-download-data/projection/mavg/tas/rcp26/2020_2039/21.1458$cckp$79.0882/21.1458$cckp$79.0882
+    # temps stores the predicted temperature
+    latitude = str(request.form.get('latitude'))
+    longitude = str(request.form.get('longitude'))
+    URL = "https://climateknowledgeportal.worldbank.org/api/data/get-download-data/projection/mavg/tas/rcp26/2020_2039/" + \
+        latitude+"$cckp$"+longitude + "/"+latitude + "$cckp$"+longitude + ""
+    resp = requests.get(url=URL)
+    decoded = resp.content.decode("utf-8")
+    cr = csv.reader(decoded.splitlines(), delimiter=',')
+    my_list = list(cr)
+    temps = []
+    for index, row in enumerate(my_list):
+        if index == 0:
+            continue
+        if index > 13:
+            break
+        temps.append(row[0])
+    print(temps)
+
+    # Do the prediction here using Classifier clf.
+
+    # Making the response message
+    response = {
+        "predict": [
+            {
+                "crop": "Predicted Crop 1",
+                "yield_percent": 90
+            },
+            {
+                "crop": "Predicted Crop 2",
+                "yield_percent": 50
+            }
+        ]
+    }
+    return jsonify(response)
+
 
 if __name__ == '__main__':
+    clf = joblib.load('../Models/saved_model.sav')
     app.run()
